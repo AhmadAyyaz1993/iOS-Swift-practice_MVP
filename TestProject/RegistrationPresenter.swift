557e90eed72170b2ba66220545019d8b97778abe
Ahmad <ahmad.ayyaz1993@gmail.com>
2018-08-11 18:32:18 +0500

    loader added

--- TestProject/RegistrationPresenter.swift
+++ TestProject/RegistrationPresenter.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+protocol  RegistrationDelegate {
+    func showProgress();
+    func hideProgress();
+    func registrationDidSucceed()
+    func registrationDidFailed(message: String)
+}
+
+class RegistrationPresenter{
+    var delegate: RegistrationDelegate
+    
+    init(delegate: RegistrationDelegate) {
+        self.delegate = delegate
+    }
+    
+    func registerUser(email: String, password: String, fullName: String, phoneNumber: String)  {
+        if email.isEmpty{
+            print("email empty")
+            self.delegate.registrationDidFailed(message: "Email cannot be empty")
+            return
+        }
+        if password.isEmpty{
+            print("password empty")
+            self.delegate.registrationDidFailed(message: "Password cannot be empty")
+            return
+        }
+        if fullName.isEmpty{
+            print("full name empty")
+            self.delegate.registrationDidFailed(message: "Name cannot be empty")
+            return
+        }
+        if phoneNumber.isEmpty{
+            print("phone number is empty")
+            self.delegate.registrationDidFailed(message: "Phone number cannot be empty")
+            return
+        }
+        
+        self.delegate.showProgress()
+        DispatchQueue.main.asyncAfter(deadline: .now()+3) {
+            self.delegate.hideProgress()
+            self.delegate.registrationDidSucceed()
+        }
+    }
+}
+
+
+
--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var phoneNumberField: UITextField!
+    @IBOutlet weak var fullNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    var presenter: RegistrationPresenter?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.presenter = RegistrationPresenter(delegate: self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +28,36 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func registerButton(_ sender: Any) {
+        self.presenter?.registerUser(email: emailField.text!, password: passwordField.text!, fullName: fullNameField.text!, phoneNumber: phoneNumberField.text!)
+    }
+    func alert() -> UIAlertController {
+        let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
+        
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        loadingIndicator.startAnimating();
+        
+        alert.view.addSubview(loadingIndicator)
+        return alert
+    }
+    
 }
 
+extension ViewController: RegistrationDelegate
+{
+    func showProgress(){
+        present(alert(), animated: true, completion: nil)
+    }
+    func hideProgress(){
+        dismiss(animated: false, completion: nil)
+    }
+    func registrationDidSucceed(){
+        print("succeed")
+    }
+    func registrationDidFailed(message: String){
+        print(message)
+    }
+    
+}
